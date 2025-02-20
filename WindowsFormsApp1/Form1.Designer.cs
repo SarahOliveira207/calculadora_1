9d3f24373d7fe4d6f38d01a669aa03e51c1f9b76
sarahOliveira-tech <sarinhalinda086@gmail.com>
2025-02-19 21:30:29 -0300

    Adicionar arquivos de projeto.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.labResultado = new System.Windows.Forms.Label();
+            this.labOper = new System.Windows.Forms.Label();
+            this.numOper1 = new System.Windows.Forms.NumericUpDown();
+            this.numOper2 = new System.Windows.Forms.NumericUpDown();
+            this.btSoma = new System.Windows.Forms.Button();
+            this.btSub = new System.Windows.Forms.Button();
+            this.btMult = new System.Windows.Forms.Button();
+            this.btDiv = new System.Windows.Forms.Button();
+            this.btIgual = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numOper1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOper2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(138, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(152, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Operando 1:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(138, 199);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Operando 2:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // labResultado
+            // 
+            this.labResultado.BackColor = System.Drawing.SystemColors.Window;
+            this.labResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labResultado.Location = new System.Drawing.Point(12, 367);
+            this.labResultado.Name = "labResultado";
+            this.labResultado.Size = new System.Drawing.Size(776, 43);
+            this.labResultado.TabIndex = 2;
+            this.labResultado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labOper
+            // 
+            this.labOper.AutoSize = true;
+            this.labOper.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labOper.Location = new System.Drawing.Point(509, 123);
+            this.labOper.Name = "labOper";
+            this.labOper.Size = new System.Drawing.Size(34, 29);
+            this.labOper.TabIndex = 3;
+            this.labOper.Text = "...";
+            this.labOper.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // numOper1
+            // 
+            this.numOper1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numOper1.Location = new System.Drawing.Point(309, 43);
+            this.numOper1.Maximum = new decimal(new int[] {
+            -1486618624,
+            232830643,
+            0,
+            0});
+            this.numOper1.Minimum = new decimal(new int[] {
+            -1304428544,
+            434162106,
+            542,
+            -2147483648});
+            this.numOper1.Name = "numOper1";
+            this.numOper1.Size = new System.Drawing.Size(131, 36);
+            this.numOper1.TabIndex = 5;
+            this.numOper1.ValueChanged += new System.EventHandler(this.numOper1_ValueChanged);
+            // 
+            // numOper2
+            // 
+            this.numOper2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numOper2.Location = new System.Drawing.Point(309, 199);
+            this.numOper2.Maximum = new decimal(new int[] {
+            -1486618624,
+            232830643,
+            0,
+            0});
+            this.numOper2.Minimum = new decimal(new int[] {
+            -1304428544,
+            434162106,
+            542,
+            -2147483648});
+            this.numOper2.Name = "numOper2";
+            this.numOper2.Size = new System.Drawing.Size(131, 36);
+            this.numOper2.TabIndex = 6;
+            // 
+            // btSoma
+            // 
+            this.btSoma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSoma.Location = new System.Drawing.Point(191, 112);
+            this.btSoma.Name = "btSoma";
+            this.btSoma.Size = new System.Drawing.Size(50, 50);
+            this.btSoma.TabIndex = 7;
+            this.btSoma.Text = "+";
+            this.btSoma.UseVisualStyleBackColor = true;
+            this.btSoma.Click += new System.EventHandler(this.btSoma_Click);
+            // 
+            // btSub
+            // 
+            this.btSub.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSub.Location = new System.Drawing.Point(264, 112);
+            this.btSub.Name = "btSub";
+            this.btSub.Size = new System.Drawing.Size(50, 50);
+            this.btSub.TabIndex = 8;
+            this.btSub.Text = "-";
+            this.btSub.UseVisualStyleBackColor = true;
+            this.btSub.Click += new System.EventHandler(this.btSub_Click);
+            // 
+            // btMult
+            // 
+            this.btMult.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMult.Location = new System.Drawing.Point(429, 112);
+            this.btMult.Name = "btMult";
+            this.btMult.Size = new System.Drawing.Size(50, 50);
+            this.btMult.TabIndex = 9;
+            this.btMult.Text = "x";
+            this.btMult.UseVisualStyleBackColor = true;
+            this.btMult.Click += new System.EventHandler(this.btMult_Click);
+            // 
+            // btDiv
+            // 
+            this.btDiv.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDiv.Location = new System.Drawing.Point(346, 112);
+            this.btDiv.Name = "btDiv";
+            this.btDiv.Size = new System.Drawing.Size(50, 50);
+            this.btDiv.TabIndex = 11;
+            this.btDiv.Text = "÷";
+            this.btDiv.UseVisualStyleBackColor = true;
+            this.btDiv.Click += new System.EventHandler(this.btDiv_Click);
+            // 
+            // btIgual
+            // 
+            this.btIgual.Enabled = false;
+            this.btIgual.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btIgual.Location = new System.Drawing.Point(266, 280);
+            this.btIgual.Name = "btIgual";
+            this.btIgual.Size = new System.Drawing.Size(174, 50);
+            this.btIgual.TabIndex = 13;
+            this.btIgual.Text = "=";
+            this.btIgual.UseVisualStyleBackColor = true;
+            this.btIgual.Click += new System.EventHandler(this.btIgual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 450);
+            this.Controls.Add(this.btIgual);
+            this.Controls.Add(this.btDiv);
+            this.Controls.Add(this.btMult);
+            this.Controls.Add(this.btSub);
+            this.Controls.Add(this.btSoma);
+            this.Controls.Add(this.numOper2);
+            this.Controls.Add(this.numOper1);
+            this.Controls.Add(this.labOper);
+            this.Controls.Add(this.labResultado);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Calculadora";
+            ((System.ComponentModel.ISupportInitialize)(this.numOper1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numOper2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label labResultado;
+        private System.Windows.Forms.Label labOper;
+        private System.Windows.Forms.NumericUpDown numOper1;
+        private System.Windows.Forms.NumericUpDown numOper2;
+        private System.Windows.Forms.Button btSoma;
+        private System.Windows.Forms.Button btSub;
+        private System.Windows.Forms.Button btMult;
+        private System.Windows.Forms.Button btDiv;
+        private System.Windows.Forms.Button btIgual;
+    }
+}
+
